5b3a3557258532d0cec9f835a1e7779294d44395
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2023-02-04 09:16:16 -0800

    Use new arcam_fmj branch to handle device queries (#63)

--- server/arcam_state_handler.py
+++ server/arcam_state_handler.py
@@ -1,7 +1,7 @@
 import asyncio
 import threading
 
-from arcam_fmj.src.arcam.fmj import SA10SourceCodes
+from arcam_fmj.src.arcam.fmj import SourceCodes, ApiModel
 from arcam_fmj.src.arcam.fmj.client import Client
 from arcam_fmj.src.arcam.fmj.client import ClientContext
 from arcam_fmj.src.arcam.fmj.state import State
@@ -20,27 +20,27 @@ class ArcamStateHandler:
 
     def source_to_str(self, source):
         return {
-            SA10SourceCodes.PHONO: "PHONO",
-            SA10SourceCodes.AUX: "AUX",
-            SA10SourceCodes.PVR: "PVR",
-            SA10SourceCodes.AV: "AV",
-            SA10SourceCodes.STB: "STB",
-            SA10SourceCodes.CD: "CD",
-            SA10SourceCodes.BD: "BD",
-            SA10SourceCodes.SAT: "SAT"
+            1: "PHONO",
+            2: "AUX",
+            3: "PVR",
+            4: "AV",
+            5: "STB",
+            6: "CD",
+            7: "BD",
+            8: "SAT",
         }.get(source)
 
 
     def str_to_source(self, source):
         return {
-            "PHONO": SA10SourceCodes.PHONO,
-            "AUX": SA10SourceCodes.AUX,
-            "PVR": SA10SourceCodes.PVR,
-            "AV": SA10SourceCodes.AV,
-            "STB": SA10SourceCodes.STB,
-            "CD": SA10SourceCodes.CD,
-            "BD": SA10SourceCodes.BD,
-            "SAT": SA10SourceCodes.SAT
+            "PHONO": SourceCodes.PHONO,
+            "AUX": SourceCodes.AUX,
+            "PVR": SourceCodes.PVR,
+            "AV": SourceCodes.AV,
+            "STB": SourceCodes.STB,
+            "CD": SourceCodes.CD,
+            "BD": SourceCodes.BD,
+            "SAT": SourceCodes.SAT
         }.get(source)
     
     def metric_to_function(self, metric):
@@ -54,19 +54,19 @@ class ArcamStateHandler:
     async def health_check(self):
         try:
             async with ClientContext(self.client):
-                state = State(self.client, self.zone)
-                power = await state.get_power(use_state=False),
-                mute = await state.get_mute(use_state=False),
-                volume = await state.get_volume(use_state=False),
+                state = State(self.client, self.zone, api_model=ApiModel.APISA_SERIES)
+                power = await state.get_power()
+                mute = await state.get_mute()
+                volume = await state.get_volume()
                 source = self.source_to_str(
-                        await state.get_source(use_state=False)
+                        await state.get_source()
                     )
                 await self.client.stop()
                 return {
                     "success": True,
-                    "power": power[0],
-                    "mute": mute[0],
-                    "volume": volume[0],
+                    "power": power,
+                    "mute": mute,
+                    "volume": volume,
                     "source": source
                 }
         except Exception:
@@ -79,8 +79,8 @@ class ArcamStateHandler:
         success = True
         try:
             async with ClientContext(self.client):
-                state = State(self.client, self.zone)
-                await state.set_mute(value, use_rc5=False)
+                state = State(self.client, self.zone, api_model=ApiModel.APISA_SERIES)
+                await state.set_mute(value)
                 await self.client.stop()
         except Exception:
             success = False
@@ -94,8 +94,8 @@ class ArcamStateHandler:
         success = True
         try:
             async with ClientContext(self.client):
-                state = State(self.client, self.zone)
-                await state.set_power(value, use_rc5=False)
+                state = State(self.client, self.zone, api_model=ApiModel.APISA_SERIES)
+                await state.set_power(value)
                 await self.client.stop()
         except Exception:
             logger.exception("error setting power state")
@@ -109,7 +109,7 @@ class ArcamStateHandler:
         success = True
         try:
             async with ClientContext(self.client):
-                state = State(self.client, self.zone)
+                state = State(self.client, self.zone, api_model=ApiModel.APISA_SERIES)
                 await state.set_volume(value)
                 await self.client.stop()
         except Exception:
@@ -147,10 +147,9 @@ class ArcamStateHandler:
         success = True
         try:
             async with ClientContext(self.client):
-                state = State(self.client, self.zone)
+                state = State(self.client, self.zone, api_model=ApiModel.APISA_SERIES)
                 await state.set_source(
-                    self.str_to_source(source_as_str),
-                    use_rc5=False
+                    self.str_to_source(source_as_str)
                 )
                 await self.client.stop()
         except Exception:
@@ -165,6 +164,6 @@ class ArcamStateHandler:
         function = self.metric_to_function(metric)
         if function is None:
             return {
-                "success": success
+                "success": False
             }
         return await function(value)
