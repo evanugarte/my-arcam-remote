d686cf0e94f45187e398f74b118e9893e865af80
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2023-02-12 10:33:02 -0800

    Make icons change color with dark mode (#64)
    * rename #2e2e2e -> Gray
    
    * revert changes to website background
    
    * add a <g> wrapper
    
    * formatting, remove g wrapper for power

--- website/src/icons/Minus.svelte
+++ website/src/icons/Minus.svelte
@@ -1,16 +1,18 @@
 <svg
     viewBox="0 -6 16 16"
-    fill="#2e2e2e"
+    fill="Gray"
     id="meteor-icon-kit__solid-minus-s"
     xmlns="http://www.w3.org/2000/svg"
     height="24px"
     width="24px"
     style="padding: 1rem;"
 >
-    <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M2 3.5H14C14.8284 3.5 15.5 2.8284 15.5 2C15.5 1.1716 14.8284 0.5 14 0.5H2C1.17157 0.5 0.5 1.1716 0.5 2C0.5 2.8284 1.17157 3.5 2 3.5z"
-        fill="#"
-    />
+    <g fill="#000000">
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M2 3.5H14C14.8284 3.5 15.5 2.8284 15.5 2C15.5 1.1716 14.8284 0.5 14 0.5H2C1.17157 0.5 0.5 1.1716 0.5 2C0.5 2.8284 1.17157 3.5 2 3.5z"
+            fill="#"
+        />
+    </g>
 </svg>
